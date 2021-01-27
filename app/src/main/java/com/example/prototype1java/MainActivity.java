7ea523e7849a4e2347a3c2ccfa25784520e84e97
Fred Amirfaiz <fred@workwithsupply.com>
2021-01-27 10:56:51 -0800

    UI Animation prototype

--- app/src/main/java/com/example/prototype1java/MainActivity.java
+++ app/src/main/java/com/example/prototype1java/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_15inchland);
+        setContentView(R.layout.activity_main);
         PathView path_view = findViewById(R.id.path);
         path_view.init();
         SetupVideo();
